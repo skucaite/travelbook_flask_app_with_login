5a766c2a5344aaf19f78e52d64c3ecade6bedeee
Jurgita Skucaite <jur.skucaite@gmail.com>
2020-06-09 19:21:01 +0300

    User Account profile

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import secrets
+from PIL import Image
 import sys
 import babel
 from flask import Flask, render_template, url_for, flash, request, redirect, abort   # jsonify
@@ -90,11 +92,45 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+# If we want just save picture:
+    # form_picture.save(picture_path)
+
+# This will resize picture (don't forget to install Pillow):
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
+    form = GuideForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.name = form.name.data
+        current_user.surname = form.surname.data
+        current_user.phone = form.phone.data
+        current_user.email = form.email.data
+        current_user.update()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.name.data = current_user.name
+        form.surname.data = current_user.surname
+        form.phone.data = current_user.phone
+        form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
 
 # ----------------------------------------------------------------#
 #  Guides
--- forms.py
+++ forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, DateField, TextAreaField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
@@ -58,6 +60,16 @@ class GuideForm(FlaskForm):
         validators=[DataRequired()])
     image_file = StringField(
         'image_file')
+    picture = FileField(
+        'Update Profile Picture',
+        validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = Guide.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
 
 
 class TravelForm(FlaskForm):
