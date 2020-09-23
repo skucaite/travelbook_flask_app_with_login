bfb740fc156f3ba3e7a8568348e1b8fe656a1069
Jurgita Skucaite <jur.skucaite@gmail.com>
2020-09-23 11:30:34 +0300

    Update routes.py

--- travelbook/travels/routes.py
+++ travelbook/travels/routes.py
@@ -1,73 +1,39 @@
-from flask import render_template, url_for, flash, request, redirect, abort, Blueprint
-from flask_login import login_user, current_user, login_required
-from travelbook import db
-from travelbook.models import Travel
-from travelbook.travels.forms import TravelForm
+from collections import Counter
 
 
-travels = Blueprint('travels', __name__)
+s = 'abba'
+
+# count = 0
+# arr = []
+
+# dic = Counter(s)
+
+# for i in dic:
+#   if dic[i] % 2 == 0:
+#     count += (dic[i]//2)
+#     arr.append(i)
 
 
-# Show Travel
-# ----------------------------------------------------------------#
-@travels.route('/travels/<travel_id>')
-def show_travel(travel_id):
-    travel = Travel.query.get_or_404(travel_id)
-    title = 'Trip: ' + travel.title
-    return render_template('show_travel.html', travel=travel, title=title)
+# print(Counter(s))
+# print(count)
 
-# Create Travel
-# ----------------------------------------------------------------#
-@travels.route('/travels/create', methods=['GET', 'POST'])
-@login_required
-def create_travel():
-    form = TravelForm()
-    if form.validate_on_submit():
-        travel = Travel(title = form.title.data,
-                        content = form.content.data,
-                        guide = current_user)
-        try:
-            travel.insert()
-            flash('Trip ' + form.title.data  + ' was successfully created!', 'success')
-            return redirect('/travels')
-        except:
-            flash('An error occurred. Trip could not be created.', 'danger')
-    return render_template('travel.html', title='New Travel', form=form, legend='New Travel')
+sum = 0
+for i, el in enumerate(s):
+  j = 1
+  B = Counter(s[i+1:])
+  while j < len(s):
+    A = Counter(el)
+    print('el: ' + el)
+    print(A)
+    print(B)
+    print(A-B)
+    if A - B == {}:
+      sum +=1
+      print('sum: ' + str(sum))
+    j += 1
+    el = "".join(s[i:j])
 
-# Update Travel
-# ----------------------------------------------------------------#
-@travels.route('/travels/<travel_id>/edit', methods=['GET', 'POST'])
-@login_required
-def edit_travel(travel_id):
-    travel=Travel.query.get_or_404(travel_id)
-    if travel.guide != current_user:
-        abort(403)
-    form = TravelForm()
-    if form.validate_on_submit():
-        travel.title = form.title.data
-        travel.content = form.content.data
-        try:
-            travel.update()
-            flash('Your travel has been updated!', 'success')
-            return redirect(url_for('travels.show_travel', travel_id=travel.id))
-        except:
-            flash('An error occurred. Trip could not be updated.', 'danger')
-    elif request.method == 'GET':
-        form.title.data = travel.title
-        form.content.data = travel.content
-    title = 'Trip ' + travel.title
 
-    return render_template('travel.html', form=form, travel=travel,
-                                title=title, legend='Update Travel')
+# print(Counter(i) - Counter(s[s.index(i)+1:]))
 
-# Delete Travel
-# ----------------------------------------------------------------#
-@travels.route('/travels/<travel_id>/delete', methods=['POST', 'DELETE'])
-def delete_travel(travel_id):
-    travel = Travel.query.get_or_404(travel_id)
-    try:
-        travel.delete()
-        flash('Deleted!', 'success')
-    except:
-        flash('There was a problem deleting that guide', 'danger')
-    return redirect(url_for('main.travels'))
+print(sum)
